e86bd41939231ac1c56ea97ae239d1b5a3a8d55f
noney1412 <nonpanpila@gmail.com>
2017-09-23 16:55:32 +0700

    RegistMove
    RegistMoveSkill

--- tryList.js
+++ tryList.js
@@ -1,1 +1,14 @@
 console.log('Hello Try');
+
+/* Stage 1 : Styled-Components Skill Day 1-2 */
+const Moves_Folder = () => {
+    console.log('Prepare My skill Here');
+}
+
+
+
+
+
+
+/* Task-List*/
+Moves_Folder()
